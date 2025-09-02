604643ce93162efe9c7dde595fb7d53fc546508f
R3FA <faris.skopak87@gmail.com>
2025-09-02 16:16:10 +0200

    Remove unnecessary try catch in Program.cs

--- JoyModels.API/src/Program.cs
+++ JoyModels.API/src/Program.cs
@@ -1,20 +1,13 @@
 using JoyModels.API.Setups;
 
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.InitializeDatabaseServices(builder.Configuration);
-    builder.Services.InitializeDependencyInjectionServices();
+builder.Services.InitializeDatabaseServices(builder.Configuration);
+builder.Services.InitializeDependencyInjectionServices();
 
-    var app = builder.Build();
-    app.UseHttpsRedirection();
-    app.UseExceptionHandler();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.Run();
-}
-catch (Exception ex)
-{
-    throw new Exception("An error occured", ex);
-}
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseExceptionHandler();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
