6081c05f309563690b66f7a31c0b092aa7c5003f
R3FA <faris.skopak87@gmail.com>
2025-09-02 16:20:43 +0200

    Rewrite logic of WebApplication just as DI setup

--- JoyModels.API/src/Program.cs
+++ JoyModels.API/src/Program.cs
@@ -5,9 +5,4 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.InitializeDatabaseServices(builder.Configuration);
 builder.Services.InitializeDependencyInjectionServices();
 
-var app = builder.Build();
-app.UseHttpsRedirection();
-app.UseExceptionHandler();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+builder.Build().Create();
