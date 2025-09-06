9895476dbd1cedb6ba55ac3b96635ec9d0313d81
R3FA <faris.skopak87@gmail.com>
2025-09-06 14:15:30 +0200

    Add Password validation - Minor changes when throwing exceptions

--- JoyModels.Services/src/Validation/RegularExpressionValidation.cs
+++ JoyModels.Services/src/Validation/RegularExpressionValidation.cs
@@ -7,11 +7,22 @@ public static class RegularExpressionValidation
     public static bool IsEmailValid(string email)
     {
         if (string.IsNullOrWhiteSpace(email))
-            return false;
+            throw new ApplicationException("Email cannot be empty string");
 
         const string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
-
         if (!Regex.IsMatch(email, pattern))
+            return false;
+
+        return true;
+    }
+
+    public static bool IsPasswordValid(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+            throw new ApplicationException("Password cannot be empty string");
+
+        const string pattern = @"^(?=.{8,})(?=.\p{Lu})(?=.\p{Nd})(?=.[!@#$%^&])[\p{L}\p{Nd}!@#$%^&*]+";
+        if (!Regex.IsMatch(password, pattern))
             return false;
 
         return true;
@@ -20,10 +31,9 @@ public static class RegularExpressionValidation
     public static bool IsStringValid(string input, int inputLength)
     {
         if (string.IsNullOrWhiteSpace(input))
-            return false;
+            throw new ApplicationException("Input cannot be empty string");
 
         var pattern = $@"^\p{{Lu}}\p{{Ll}}{{1,{inputLength - 1}}}$";
-
         if (!Regex.IsMatch(input, pattern))
             return false;
 
