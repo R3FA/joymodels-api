346de7d84ec59e6b6a36b005c1334aedce80c76f
R3FA <faris.skopak87@gmail.com>
2025-09-06 17:18:32 +0200

    Move regex validation methods to another file

---
+++ JoyModels.Services/src/Services/Sso/SsoHelperMethods.cs
@@ -0,0 +1,33 @@
+using JoyModels.Models.DataTransferObjects.User;
+using JoyModels.Services.Validation;
+
+namespace JoyModels.Services.Services.Sso;
+
+public static class SsoHelperMethods
+{
+    public static void ValidateUserCreation(this UserCreate user)
+    {
+        if (!RegularExpressionValidation.IsStringValid(user.FirstName,
+                Validation.ConstantValidation.User.UserCreate.FirstNameMaxLength))
+            throw new ArgumentException($"First name `{user.FirstName}` is invalid");
+
+        if (user.LastName != null)
+        {
+            if (!RegularExpressionValidation.IsStringValid(user.LastName,
+                    Validation.ConstantValidation.User.UserCreate.LastNameMaxLength))
+                throw new ArgumentException($"Last name `{user.LastName}` is invalid");
+        }
+
+        if (!RegularExpressionValidation.IsNicknameValid(user.NickName,
+                Validation.ConstantValidation.User.UserCreate.NickNameMaxLength))
+            throw new ArgumentException($"Nickname `{user.NickName}` is invalid");
+
+        if (!RegularExpressionValidation.IsEmailValid(user.Email,
+                Validation.ConstantValidation.User.UserCreate.EmailMaxLength))
+            throw new ArgumentException($"Email `{user.Email}` is invalid");
+
+        if (!RegularExpressionValidation.IsPasswordValid(user.Password,
+                Validation.ConstantValidation.User.UserCreate.PasswordMaxLength))
+            throw new ArgumentException($"Password `{user.Password}` is invalid");
+    }
+}
