c23fb2919a2010fde35c84bdbb8640e407ad1cb2
Faris <77499895+R3FA@users.noreply.github.com>
2025-09-12 22:25:40 +0200

    Fix for redundant drop and insert of the same user roles after every migration (#8)
    * Minor user roles fixes in ModelSnapshot
    
    * Remove duplicated user roles entries from .Designer.cs
    
    * Add user roles uuids as intended

--- JoyModels.Models/src/Database/Migrations/20250906194729_RemovePasswordSaltCol.Designer.cs
+++ JoyModels.Models/src/Database/Migrations/20250906194729_RemovePasswordSaltCol.Designer.cs
@@ -719,36 +719,36 @@ namespace JoyModels.Models.src.Database.Migrations
                         .IsUnique();
 
                     b.ToTable("user_roles", (string)null);
-
+                    
                     b.HasData(
                         new
                         {
-                            Uuid = new Guid("9d532540-9d12-41be-88a2-4fe750f73972"),
+                            Uuid = new Guid("6aa5d268-c259-4054-b604-ff545eaa2f1e"),
                             RoleName = "Undefined"
                         },
                         new
                         {
-                            Uuid = new Guid("3c093d83-18eb-41a4-9d62-c8d34e63d83c"),
+                            Uuid = new Guid("2107d612-ac38-4390-aeb6-276cf55b42bb"),
                             RoleName = "Unverified"
                         },
                         new
                         {
-                            Uuid = new Guid("b2d041cc-438b-49dc-9e2e-034f0cd88af2"),
+                            Uuid = new Guid("efd09239-1461-4740-86ae-94ed49137a36"),
                             RoleName = "User"
                         },
                         new
                         {
-                            Uuid = new Guid("fee0de0a-f25d-4c5e-bb38-253c47e1964d"),
+                            Uuid = new Guid("90051a72-89ea-48ca-8543-43f5843148c8"),
                             RoleName = "Helper"
                         },
                         new
                         {
-                            Uuid = new Guid("3758f6b0-834f-4cd5-8725-ef718639e5b7"),
+                            Uuid = new Guid("44b4be39-2884-462a-98c0-cb3b5eb9c3dd"),
                             RoleName = "Admin"
                         },
                         new
                         {
-                            Uuid = new Guid("03ee6412-91e7-4352-a614-23b09a779814"),
+                            Uuid = new Guid("c5c8ab92-5933-4122-a804-6533516aeb5d"),
                             RoleName = "Root"
                         });
                 });
