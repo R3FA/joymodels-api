04bf5372e9199f7a77ec9a01ac4be558025b7baf
R3FA <faris.skopak87@gmail.com>
2025-10-20 18:35:37 +0200

    Changes in EmailSendRequest DTO

--- JoyModels.Models/src/DataTransferObjects/RequestTypes/Email/EmailSendRequest.cs
+++ JoyModels.Models/src/DataTransferObjects/RequestTypes/Email/EmailSendRequest.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JoyModels.Models.DataTransferObjects.RequestTypes.Email;
 
 public class EmailSendRequest
 {
-    public string To { get; set; } = string.Empty;
-    public string Subject { get; set; } = string.Empty;
-    public string Body { get; set; } = string.Empty;
+    [Required] public string To { get; set; } = string.Empty;
+    public string? Subject { get; set; } = string.Empty;
+    [Required] public string Body { get; set; } = string.Empty;
 }
