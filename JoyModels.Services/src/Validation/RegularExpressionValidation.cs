63f7b4f3dba5df5f96623130ec376b36366632b3
R3FA <faris.skopak87@gmail.com>
2025-11-25 17:13:05 +0100

    Minor changes in RegularExpressionValidation.cs

--- JoyModels.Services/src/Validation/RegularExpressionValidation.cs
+++ JoyModels.Services/src/Validation/RegularExpressionValidation.cs
@@ -12,7 +12,7 @@ public static class RegularExpressionValidation
         const string pattern = @"^(?=.{1,255})[^@\s]+@[^@\s]+\.[^@\s]+$";
 
         if (!Regex.IsMatch(email, pattern))
-            throw new ArgumentException("Invalid format. The value must be a valid email address without spaces");
+            throw new ArgumentException("Invalid format. The value must be a valid email address without spaces.");
     }
 
     public static void ValidatePassword(string password)
@@ -24,37 +24,37 @@ public static class RegularExpressionValidation
 
         if (!Regex.IsMatch(password, pattern))
             throw new ArgumentException(
-                "Password must be minimum 8 characters long and include at least one uppercase letter, one number, and one special character (@$!%*#?&).");
+                "Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character (@$!%*#?&).");
     }
 
     public static void ValidateNickname(string nickname)
     {
         if (string.IsNullOrWhiteSpace(nickname))
-            throw new ArgumentException("Nickname cannot be empty string");
+            throw new ArgumentException("Nickname cannot be empty string.");
 
         const string pattern = @"^[\p{Ll}\p{Nd}]{3,}$";
 
         if (!Regex.IsMatch(nickname, pattern))
             throw new ArgumentException(
-                "The value must be minimum 3 characters long and contain only lowercase letters and numbers.");
+                "Nickname must be at least 3 characters long and contain only lowercase letters and numbers.");
     }
 
     public static void ValidateName(string name)
     {
         if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("Name cannot be empty string");
+            throw new ArgumentException("Name cannot be empty string.");
 
         const string pattern = @"^\p{Lu}\p{Ll}{1,}$";
 
         if (!Regex.IsMatch(name, pattern))
             throw new ArgumentException(
-                "First name must begin with a capital letter and contain only lowercase letters after.");
+                "First or Last name must begin with a capital letter and contain only lowercase letters after.");
     }
 
     public static void ValidateOtpCode(string otpCode)
     {
         if (string.IsNullOrWhiteSpace(otpCode))
-            throw new ArgumentException("OTP Code cannot be empty string");
+            throw new ArgumentException("OTP Code cannot be empty string.");
 
         const string pattern = "^[A-Z0-9]{12}$";
 
@@ -66,7 +66,7 @@ public static class RegularExpressionValidation
     public static void ValidateText(string text)
     {
         if (string.IsNullOrWhiteSpace(text))
-            throw new ArgumentException("Input cannot be empty string");
+            throw new ArgumentException("Input cannot be empty string.");
 
         const string pattern = @"^[\p{L}\p{Nd}:.,\-\' ]{1,}$";
 
