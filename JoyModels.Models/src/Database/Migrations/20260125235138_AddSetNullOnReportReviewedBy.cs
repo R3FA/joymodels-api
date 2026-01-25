39b1c8f3ccc181ce23a7980d824c0e608a402914
R3FA <faris.skopak87@gmail.com>
2026-01-26 00:55:53 +0100

    Cascade delete fix for review_by_uuid

---
+++ JoyModels.Models/src/Database/Migrations/20260125235138_AddSetNullOnReportReviewedBy.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JoyModels.Models.src.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSetNullOnReportReviewedBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "reports_ibfk_2",
+                table: "reports");
+
+            migrationBuilder.AddForeignKey(
+                name: "reports_ibfk_2",
+                table: "reports",
+                column: "reviewed_by_uuid",
+                principalTable: "users",
+                principalColumn: "uuid",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "reports_ibfk_2",
+                table: "reports");
+
+            migrationBuilder.AddForeignKey(
+                name: "reports_ibfk_2",
+                table: "reports",
+                column: "reviewed_by_uuid",
+                principalTable: "users",
+                principalColumn: "uuid");
+        }
+    }
+}
